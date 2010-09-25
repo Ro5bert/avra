26e8602eab652a12db5b9a779e71631d18f20ff7
Marcin Cieslak <saper@saper.info>
2010-09-25 03:08:13 +0200

    Another CR+LF fix

--- examples/testcode_avra-1_2_3.asm
+++ examples/testcode_avra-1_2_3.asm
@@ -1,5 +1,6 @@
 	.device ATmega8
-	.org	0  	.db	1,2
+	.org	0  
+	.db	1,2
 	.message "The previous line is ignored with avra-1.2.2 because .org 0 is terminated with CR only. "
 	.message "This is line 5 but avra-1.2.2 shows line 4"
 	.db 	"X%MINUTE%YEAR%"	; Take a look at this percent chars too : % % % %
