8b984541cb1a4df3f6ad697b8c7a862e2e600ba5
robert <robertrussell.72001@gmail.com>
2020-05-21 13:09:39 -0700

    Enhance runtest.sh script
    Also, update test1 and test2 accordingly.

--- tests/regression/runtests.sh
+++ tests/regression/runtests.sh
@@ -1,33 +1,69 @@
 #!/bin/sh
 
-AVRA_BIN="../../src/avra"
+AVRA="../../src/avra"
+tstcnt=0
+failcnt=0
+failed=0
+
+fail() {
+	echo "$1"
+	failed=1
+}
 
 for dir in */; do
 	base="$(basename "${dir}")"
-	sfile="${base}/${base}.asm"
-    hexfile="${base}/${base}.hex"
-    eepfile="${base}/${base}.eep.hex"
-    printf "\nTesting ${base}\n"
-	${AVRA_BIN} "${sfile}" > /dev/null || \
-		(echo "AVRA had non-zero exit status"; exit 1)
-    if [ ! -f "${hexfile}.expected" ]; then
-		echo "No expected HEX file found"
-	elif cmp "${hexfile}" "${hexfile}.expected"; then
-		echo "HEX file ok"
+	tstfile="${base}/test"
+	srcfile="${base}/test.asm"
+    hexfile="${base}/test.hex"
+    eepfile="${base}/test.eep.hex"
+
+	failed=0
+	tstcnt=$((tstcnt+1))
+    printf "\nTesting %s\n" "${base}"
+
+	# First look for test executable.
+	if [ -f "${tstfile}" ]; then
+		if [ ! -x "${tstfile}" ]; then
+			fail "${tstfile} is not executable"
+		elif ! (cd "${base}" && AVRA="../${AVRA}" ./test); then
+			fail "${tstfile} had non-zero exit status"
+		fi
+	# No test executable. Make sure test.asm exists.
+	elif [ ! -f "${srcfile}" ]; then
+		fail "Neither ${tstfile} nor ${srcfile} found"
+	# test.asm exists; compile it.
+	elif ! ${AVRA} "${srcfile}" > /dev/null; then
+		fail "AVRA had non-zero exit status"
+	# test.asm compiled successfully; check expected output.
 	else
-        printf "Different HEX file!\n\n"
-        exit 1
-    fi
-    if [ ! -f "${eepfile}.expected" ]; then
-		echo "No expected EEPROM file found"
-	elif cmp "${eepfile}" "${eepfile}.expected"; then
-		echo "EEPROM file ok"
-    else
-        printf "Different EEPROM file!\n\n"
-        exit 1
-    fi
-    rm "${hexfile}" "${eepfile}" "${base}"/*.obj
+		# Check expected hex.
+		if [ ! -f "${hexfile}.expected" ]; then
+			echo "No expected HEX file found"
+		elif cmp "${hexfile}" "${hexfile}.expected"; then
+			echo "HEX file ok"
+		else
+			fail "Different HEX file"
+		fi
+
+		# Check expected eeprom hex.
+		if [ ! -f "${eepfile}.expected" ]; then
+			echo "No expected EEPROM file found"
+		elif cmp "${eepfile}" "${eepfile}.expected"; then
+			echo "EEPROM file ok"
+		else
+			fail "Different EEPROM file"
+		fi
+
+		rm "${hexfile}" "${eepfile}" "${base}"/*.obj
+	fi
+
+	if [ $failed -eq 1 ]; then
+		echo "FAILED"
+		failcnt=$((failcnt+1))
+	else
+		echo "PASSED"
+	fi
 done
 
-printf "\nEverything ok!\n"
-exit 0
+printf "\n%d tests, %d failed\n\n" "$tstcnt" "$failcnt"
+exit $((!!failcnt))
