fa39129994942de9ba40ff3079d3b96ed9f87f9e
robert <robertrussell.72001@gmail.com>
2020-05-20 21:51:51 -0700

    Cleanup regression testing

--- tests/regression/README.md
+++ tests/regression/README.md
@@ -1,8 +1,14 @@
 # Regression tests
 
-In this folder, we test regression by having a bunch of example source file
-along with their expected HEX result. `runtests.sh` runs `avra` on each source
-file and compares the result with the expected results. Fails if different.
+Each test gets it own folder, which must contain an `x.asm` file, where `x` is
+the name of the folder.
+Each test folder can also optionally contain `x.hex.expected` and/or
+`x.eep.hex.expected`.
+When `runtests.sh` is executed, it runs `avra` on each source file and compares
+the result with the expected results (if they exists).
+`runtests.sh` fails if and only if the expected results are given and they are
+different than the actual results, or if `avra` has a non-zero exit status.
 
 Of course, it's possible that output changes are normal, due to a new feature
 or a bug fix. In these cases, expected hex files should be updated.
+
--- tests/regression/runtests.sh
+++ tests/regression/runtests.sh
@@ -2,25 +2,32 @@
 
 AVRA_BIN="../../src/avra"
 
-for sfile in *.asm; do
-    hexfile="$(basename $sfile .asm).hex"
-    eepfile="$(basename $sfile .asm).eep.hex"
-    echo "Processing ${sfile} ${hexfile}"
-    ${AVRA_BIN} ${sfile} > /dev/null
-    if cmp ${hexfile} ${hexfile}.expected; then
-        echo "HEX file ok"
-    else
-        echo "Different HEX file"
+for dir in */; do
+	base="$(basename "${dir}")"
+	sfile="${base}/${base}.asm"
+    hexfile="${base}/${base}.hex"
+    eepfile="${base}/${base}.eep.hex"
+    printf "\nTesting ${base}\n"
+	${AVRA_BIN} "${sfile}" > /dev/null || \
+		(echo "AVRA had non-zero exit status"; exit 1)
+    if [ ! -f "${hexfile}.expected" ]; then
+		echo "No expected HEX file found"
+	elif cmp "${hexfile}" "${hexfile}.expected"; then
+		echo "HEX file ok"
+	else
+        printf "Different HEX file!\n\n"
         exit 1
     fi
-    if cmp ${eepfile} ${eepfile}.expected; then
-        echo "EEPROM file ok"
+    if [ ! -f "${eepfile}.expected" ]; then
+		echo "No expected EEPROM file found"
+	elif cmp "${eepfile}" "${eepfile}.expected"; then
+		echo "EEPROM file ok"
     else
-        echo "Different EEPROM file"
+        printf "Different EEPROM file!\n\n"
         exit 1
     fi
-    rm ${hexfile} ${eepfile} *.obj
+    rm "${hexfile}" "${eepfile}" "${base}"/*.obj
 done
 
-echo "Everything ok!"
+printf "\nEverything ok!\n"
 exit 0
