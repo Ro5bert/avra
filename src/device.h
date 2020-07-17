28abae2fc16e3ef8096387501cfb127a2d6c9aa6
robert <robertrussell.72001@gmail.com>
2020-07-17 10:18:41 -0700

    Fix and cleanup device.c and device.h
    - Cleanup whitespace in device list
    - Remove useless trailing comments in device list
    - Remove some placeholder comments
    - Add ATtiny4,5,9
    - Correct ATtiny10
        - 32 bytes RAM, not none
        - Has X and Y regs
        - Isn't DF_TINY1X, but is DF_AVR8L
    - Fix ATtiny20 flash size (1k words, not 2k)
    - Clarify DF_TINY1X and DF_AVR8L flags descriptions
    - Flag field on struct device is an int, with is only guaranteed to be
      >= 16 bits, but the defines were misleadingly using 32 bit literals.
      To prevent a mistake in the future, make the literals 16-bits and add
      a comment stating that if new flags are added, then the flag field
      must be bigger (e.g., long).

--- src/device.h
+++ src/device.h
@@ -26,23 +26,28 @@
  */
 
 /* Device flags */
-#define DF_NO_MUL    0x00000001
-#define DF_NO_JMP    0x00000002	// No JMP, CALL
-#define DF_NO_XREG   0x00000004	// No X register
-#define DF_NO_YREG   0x00000008	// No Y register
-#define DF_TINY1X    0x00000010	/* AT90S1200, ATtiny10-12  set: No ADIW, SBIW,
-				   IJMP, ICALL, LDD, STD, LDS, STS, PUSH, POP */
-#define DF_NO_LPM    0x00000020	// No LPM instruction
-#define DF_NO_LPM_X  0x00000040 // No LPM Rd,Z or LPM Rd,Z+ instruction
-#define DF_NO_ELPM   0x00000080	// No ELPM instruction
-#define DF_NO_ELPM_X 0x00000100 // No ELPM Rd,Z or LPM Rd,Z+ instruction
-#define DF_NO_SPM    0x00000200 // No SPM instruction
-#define DF_NO_ESPM   0x00000400 // No ESPM instruction
-#define DF_NO_MOVW   0x00000800 // No MOVW instruction
-#define DF_NO_BREAK  0x00001000 // No BREAK instruction
-#define DF_NO_EICALL 0x00002000 // No EICALL instruction
-#define DF_NO_EIJMP  0x00004000 // No EIJMP instruction
-#define DF_AVR8L     0x00008000	/* ATtiny10, 20, 40 set No ADIW, SBIW, one word LDS/STS */
+#define DF_NO_MUL    0x0001 // No {,F}MUL{,S,SU}
+#define DF_NO_JMP    0x0002 // No JMP, CALL
+#define DF_NO_XREG   0x0004 // No X register
+#define DF_NO_YREG   0x0008 // No Y register
+#define DF_TINY1X    0x0010 /* AT90S1200, ATtiny11/12  set: No ADIW, SBIW,
+                               IJMP, ICALL, LDD, STD, LDS, STS, PUSH, POP */
+#define DF_NO_LPM    0x0020 // No LPM instruction
+#define DF_NO_LPM_X  0x0040 // No LPM Rd,Z or LPM Rd,Z+ instruction
+#define DF_NO_ELPM   0x0080 // No ELPM instruction
+#define DF_NO_ELPM_X 0x0100 // No ELPM Rd,Z or LPM Rd,Z+ instruction
+#define DF_NO_SPM    0x0200 // No SPM instruction
+#define DF_NO_ESPM   0x0400 // No ESPM instruction
+#define DF_NO_MOVW   0x0800 // No MOVW instruction
+#define DF_NO_BREAK  0x1000 // No BREAK instruction
+#define DF_NO_EICALL 0x2000 // No EICALL instruction
+#define DF_NO_EIJMP  0x4000 // No EIJMP instruction
+#define DF_AVR8L     0x8000 /* Also known as AVRrc (reduced core)?
+                               ATtiny4,5,9,10,20,40,102,104: No ADIW, SBIW;
+                               one word LDS/STS */
+/* If more flags are added, the size of the flag field in struct device must
+ * be increased! C ints are only guaranteed to be at least 16 bits, and we're
+ * currently using all of them. */
 
 struct device {
 	char *name;
