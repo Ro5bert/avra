7fbea54c7cb48db6f1ae3f9ddc8042527cb9c038
Imre Horvath <imi.horvath@gmail.com>
2023-05-27 00:41:27 +0200

    Add regression test

---
+++ tests/regression/preprocessor-macros/test.asm
@@ -0,0 +1,14 @@
+.device atmega328p
+
+#define EIGHT (1 << 3)
+#define SQR(X) ((X)*(X))
+
+ldi r16, EIGHT
+ldi r16, SQR(4)
+
+#define FOOBAR subi
+#define IMMED(X) X##i
+#define SUBI(X,Y) X ## Y
+
+IMMED(ld) r16, 1
+SUBI(FOO, BAR) r16, 1
